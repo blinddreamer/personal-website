f9ed5a9a460523382a403f01d518690a79fd970c
Nikolay Apostolov <blinddreamer@huku.rocks>
2022-03-27 21:58:44 +0300

    start all over -again

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Menu = styled.h1`
+text-align: center;
+color: palevioletred;`
+
+const Navbar = () => {
+  return (
+    <div>
+        <Menu>Service Status</Menu>
+    </div>
+  )
+}
+
+export default Navbar
