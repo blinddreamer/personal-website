93da3ce38d2c053fd0c950f5a023272ba2acbdb3
BlindDreamer <blinddreamer@huku.rocks>
2022-04-01 01:51:09 +0300

    its the final hour

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(3000);
+const publicPath = path.join(__dirname, '..', "build");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (res, req) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+})
+app.listen(port, () => console.log(`App listening at port: ${port}`));
