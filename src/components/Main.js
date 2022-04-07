3418e888090b406a14b92b3461c4344a72a9f419
Nikolay Apostolov <blinddreamer@huku.rocks>
2022-04-07 20:22:11 +0300

    fix css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,54 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
 import { SiDocker, SiGithub, SiTwitter, SiGmail } from "react-icons/si";
-
-const Box = styled.div`
-margin-left:2%;
-padding: 15% 0% 0% 0%;
-`
-const Title = styled.h1`
-color: #fff;
-font-weight: 700;
-line-height: 1.25em;
-font-size: 3.25em;
-`
-const Text = styled.p`
-color: rgba(255, 255, 255, 0.5);
-font-size: 1.35em;
-text-shadow: 0 0 2px #000; /* homework */
-`
-const Footer = styled.div`
-margin-left:2%;
-Margin: 1em 0 0 0 0;
-height:100px;
-position:fixed;
-bottom:3%;
-`
-const Links = styled.table`
-color: rgba(255, 255, 255, 0.5);
-`
-const Copy = styled.p`
-color: rgba(255, 255, 255, 0.5);
-font-size: 0.8em;
-`
 
 const Main = () => {
   return (
     
     <>
-    <Box>
-      <Title>huku.rocks</Title>
-      <Text>coming soon-ish<br></br>eventually</Text>
-    </Box>
-    <Footer>
+    <div id="box-div">
+      <h1>huku.rocks</h1>
+      <h2>coming soon-ish<br></br>eventually</h2>
+    </div>
+    <fiv id="footer">
       <table>
-        <td title="twitter"><a href="https://twitter.com/HukuA" title="twitter"><Links><SiTwitter /></Links></a></td>
-        <td title="docker"><a href="https://hub.docker.com/u/blinddreamer" title="dockerhub"><Links></Links><SiDocker /></a></td>
-        <td title="github"><a href="https://github.com/blinddreamer/" title="github"><Links><SiGithub /></Links></a></td>
-        <td title="email"><a href="mailto:blinddreamer@huku.rocks" title="email"><Links><SiGmail /></Links></a></td>
+        <td title="twitter"><a href="https://twitter.com/HukuA" title="twitter"><SiTwitter /></a></td>
+        <td title="docker"><a href="https://hub.docker.com/u/blinddreamer" title="dockerhub"><SiDocker /></a></td>
+        <td title="github"><a href="https://github.com/blinddreamer/" title="github"><SiGithub /></a></td>
+        <td title="email"><a href="mailto:blinddreamer@huku.rocks" title="email"><SiGmail /></a></td>
       </table>
-      <Copy>&copy; huku.rocks</Copy>
-    </Footer>
+      <h3>&copy; huku.rocks</h3>
+    </fiv>
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Menu = styled.div`
-font-size: 1em;
-color:rgba(255, 255, 255, 0.5);
-margin-right:10px;
-`
-const Box = styled.div`
-margin-left:2%;
-position:fixed;
-margin-top:2%;
-`
-
 
 const Navbar = () => {
   return (
-    <Box>
+    <div id="div-box">
       <table>
-        <td><Menu id="menu">home</Menu></td>
-        <td><Menu id="menu"><a href="https://monitor.huku.rocks">monitor</a></Menu></td>
+        <td><div id="menu">home</div></td>
+        <td><div id="menu"><a href="https://monitor.huku.rocks">monitor</a></div></td>
       </table>
-    </Box>
+    </div>
   )
 }
 export default Navbar
