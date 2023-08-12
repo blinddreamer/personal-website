3a25f88614556178ef5622a29ced6a4a30dcd1ef
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-08-12 15:05:21 +0300

    erm.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { SiDocker, SiGithub, SiTwitter, SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div>
-      <a href="https://twitter.com/HukuA" title="Twitter" aria-label="Twitter">
-        <SiTwitter />
-      </a>
-      <a
-        href="https://hub.docker.com/u/blinddreamer"
-        title="DockerHub"
-        aria-label="DockerHub"
-      >
-        <SiDocker />
-      </a>
-      <a
-        href="https://github.com/blinddreamer/"
-        title="GitHub"
-        aria-label="GitHub"
-      >
-        <SiGithub />
-      </a>
-      <a href="mailto:blinddreamer@huku.rocks" title="Email" aria-label="Email">
-        <SiGmail />
-      </a>
-      <br />
-      <div id="copu">&copy; huku.rocks</div>
-    </div>
+    <Fragment>
+      <table>
+        <tr>
+          <a
+            href="https://twitter.com/HukuA"
+            title="Twitter"
+            aria-label="Twitter"
+          >
+            <SiTwitter />
+          </a>
+          <a
+            href="https://hub.docker.com/u/blinddreamer"
+            title="DockerHub"
+            aria-label="DockerHub"
+          >
+            <SiDocker />
+          </a>
+          <a
+            href="https://github.com/blinddreamer/"
+            title="GitHub"
+            aria-label="GitHub"
+          >
+            <SiGithub />
+          </a>
+          <a
+            href="mailto:blinddreamer@huku.rocks"
+            title="Email"
+            aria-label="Email"
+          >
+            <SiGmail />
+          </a>
+        </tr>
+        <tr>
+          <div id="copu">&copy; huku.rocks</div>
+        </tr>
+      </table>
+    </Fragment>
   );
 };
 
